9cda45b97c97848fa70407095928bc40f4b5d158
yosaton <Yosaton@Yo.com>
2020-10-04 15:39:11 -0500

    first commit

---
+++ routes/botman.php
@@ -0,0 +1,37 @@
+<?php
+use App\Http\Controllers\BotManController;
+
+$botman = resolve('botman');
+
+// $botman->hears('Hi', function ($bot) {
+//     $bot->reply('Hello!');
+// });
+
+// $botman->hears('what is your name', function ($bot) {
+//     $bot->reply('Yosaton');
+// });
+
+$botman->hears('call me {name}', function ($bot, $name) {
+    $bot->reply('Your name is: '.$name);
+    // dd($bot->getMessage()->getPayload());
+});
+
+$botman->hears('call me {name} the {adjective}', function ($bot, $name, $adjective) {
+    $bot->reply('Hello '.$name.'. You truly are '.$adjective);
+    
+});
+
+// $storage = $botman->userStorage();
+
+// dd($storage, "storage");
+
+// $bot->userStorage()->save([
+//     'name' => $name
+// ]);
+
+$botman->fallback(function($bot) {
+    $bot->reply('Sorry, I did not understand these commands. Here is a list of commands I understand: ...');
+});
+
+
+$botman->hears('Start conversation', BotManController::class.'@startConversation');
