b3c291d4557490cf5a558f0384b9c2beaeb48448
yosaton <Yosaton@Yo.com>
2020-10-04 18:14:09 -0500

    messing with test file

--- public/test.php
+++ public/test.php
@@ -30,10 +30,10 @@ $botman->listen();
 
 
 /* GET ALL VARIABLES GET & POST */
-// foreach ($_REQUEST AS $key => $value){
-//     $message .= "$key => $value ($_SERVER[REQUEST_METHOD])\n";
-// }
-// $input = file_get_contents("php://input");
-// $array = print_r(json_decode($input, true), true);
-// file_put_contents('fbmessenger.txt', $message.$array."\nREQUEST_METHOD: $_SERVER[REQUEST_METHOD]\n----- Request Date: ".date("d.m.Y H:i:s")." IP: $_SERVER[REMOTE_ADDR] -----\n\n", FILE_APPEND);
-// echo $_REQUEST['hub_challenge'];
+foreach ($_REQUEST AS $key => $value){
+    $message .= "$key => $value ($_SERVER[REQUEST_METHOD])\n";
+}
+$input = file_get_contents("php://input");
+$array = print_r(json_decode($input, true), true);
+file_put_contents('fbmessenger.txt', $message.$array."\nREQUEST_METHOD: $_SERVER[REQUEST_METHOD]\n----- Request Date: ".date("d.m.Y H:i:s")." IP: $_SERVER[REMOTE_ADDR] -----\n\n", FILE_APPEND);
+echo $_REQUEST['hub_challenge'];
