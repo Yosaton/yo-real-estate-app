22b572989eaa3b8ff1144daa3dca9254a667dd64
yosaton <Yosaton@Yo.com>
2020-10-13 23:52:23 -0500

    test

--- public/test.php
+++ public/test.php
@@ -12,7 +12,6 @@ use BotMan\Drivers\Facebook\Extensions\ButtonTemplate;
 use BotMan\Drivers\Facebook\Extensions\ElementButton;
 use BotMan\BotMan\Messages\Conversations\Conversation;
 use BotMan\BotMan\Messages\Incoming\Answer;
-
 use BotMan\BotMan\Messages\Outgoing\Actions\Button;
 use BotMan\BotMan\Messages\Outgoing\Question;
 
@@ -76,24 +75,54 @@ class OnboardingConversation extends Conversation
 
     protected $email;
 
-    public function askForDatabase()
+    public function welcome()
 {
-    $question = Question::create('Do you need a database?')
-        ->fallback('Unable to create a new database')
-        ->callbackId('create_database')
+    $question = Question::create('Welcome to 123 Main Street Bot - I see you are looking to purchase a home.  Are you looking to buy in:')
+        ->fallback('Unable to funnel lead')
+        ->callbackId('lead_funneled')
         ->addButtons([
-            Button::create('Of course')->value('yes'),
-            Button::create('Hell no!')->value('no'),
+            Button::create('1-3 months')->value('1-3 months'),
+            Button::create('3-6 months')->value('3-6 months'),
+            Button::create('6 months +')->value('6 months +'),
         ]);
 
     $this->ask($question, function (Answer $answer) {
         // Detect if button was clicked:
         if ($answer->isInteractiveMessageReply()) {
-            $selectedValue = $answer->getValue(); // will be either 'yes' or 'no'
-            $selectedText = $answer->getText(); // will be either 'Of course' or 'Hell no!'
+            $selectedValue = $answer->getValue(); // will be either 1-3 monts, 3-6 months, or 6 months +
+            $selectedText = $answer->getText(); // will be either 1-3 monts, 3-6 months, or 6 months +
         }
+
+    $this->say('Great!');
+    $this->askPrequalified();
+        
     });
 }
+
+public function askPrequalified()
+{
+    $question = Question::create('Have you been prequalified by a mortgage loan officer yet?')
+        ->fallback('Unable to funnel lead')
+        ->callbackId('lead_funneled')
+        ->addButtons([
+            Button::create('Yes')->value('yes'),
+            Button::create('No')->value('no'),
+        ]);
+
+    $this->ask($question, function (Answer $answer) {
+        // Detect if button was clicked:
+        if ($answer->isInteractiveMessageReply()) {
+            $selectedValue = $answer->getValue(); // will be either Yes or No
+            $selectedText = $answer->getText(); // will be either yes or no
+        }
+
+    $this->say('Woohoo!');
+    // $this->askPrequalified();
+        
+    });
+}
+
+
 
 
     // public function askFirstname()
@@ -120,7 +149,7 @@ class OnboardingConversation extends Conversation
     public function run()
     {
         // This will be called immediately
-        $this->askForDatabase();
+        $this->welcome();
     }
 }
 
